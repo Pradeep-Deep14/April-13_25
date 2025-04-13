fe089b356342467d28425734d8d29b4b8dad6f60
Pradeep <deeppradeep1998@gmail.com>
2025-04-13 12:43:32 +0530

    first committ

--- 1.py
+++ 1.py
@@ -1,7 +1,9 @@
 def append_item(lst):
     lst.append(4)
-    #return lst  
+    return lst  #No need of return statement
 
 my_list = [1, 2, 3]
 append_item(my_list)
 print(my_list)  # Output: [1, 2, 3, 4]
+#with or without return statement, the output will be same
+# The function modifies the list in place, so the original list is changed.
