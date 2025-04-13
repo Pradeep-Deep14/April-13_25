a640da20b01a9aecfdc773835f944a24abdd03d2
Pradeep <deeppradeep1998@gmail.com>
2025-04-13 18:10:35 +0530

    first committ

--- 2.py
+++ 2.py
@@ -3,3 +3,7 @@ if my_list == True:
     print("The list is not empty")
 else:
     print('The list is empty')
+
+#comparing a list to true in python always returns false, so the above code will always print 'The list is empty'
+# The correct way to check if a list is empty is to use the len() function
+# or to use the not operator
